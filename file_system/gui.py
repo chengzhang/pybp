f443b821ad0aef02d383cffdf377436ec3930dae
chengzhang <zhangwpc@gmail.com>
2020-10-21 17:47:08 +0800

    lint

---
+++ file_system/gui.py
@@ -0,0 +1,48 @@
+"""operate filesystem via gui"""
+
+# coding = utf8
+
+from enum import Enum
+import argparse
+import easygui
+
+
+class ActEnum(Enum):
+    """action choices for main()"""
+    choose_file = 0
+    choose_dir = 1
+
+    def member_names(self):
+        """return a list of member name"""
+        return self._member_names_  # pylint: disable=no-member
+
+
+def choose_file():
+    """choose file via gui"""
+    filename = easygui.fileopenbox()
+    print('file you choose: {}'.format(filename))
+
+
+def choose_dir():
+    """choose dir via gui"""
+    path = easygui.diropenbox()
+    print('path you choose: {}'.format(path))
+
+
+def parse_args():
+    """parse args for main()"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--act",
+                        type=str,
+                        choices=ActEnum.member_names,
+                        default=ActEnum.choose_file.name)
+    main_args = parser.parse_args()
+    return main_args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.act == ActEnum.choose_file.name:
+        choose_file()
+    elif args.act == ActEnum.choose_dir.name:
+        choose_dir()
