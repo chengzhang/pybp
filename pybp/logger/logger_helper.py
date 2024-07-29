a03bec48cc50e50f301b2e939e0f439b104e3180
donyzhang <donyzhang@tencent.com>
2024-07-29 20:26:30 +0800

    import fix

--- pybp/logger/logger_helper.py
+++ pybp/logger/logger_helper.py
@@ -9,7 +9,7 @@ import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
-from utils.singleton import singleton
+from pybp.design_pattern.singleton import singleton
 
 
 APP_NAME = os.getenv('APP_NAME', 'UNDEFINED_APP')
